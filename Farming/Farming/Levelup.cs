300ed9dce99bc484e9872d718acf93d6f95b57b6
TheRandoman123 <nerfsteven@gmail.com>
2018-09-16 21:44:15 -0400

    1.0
    Full version of the game

---
+++ Farming/Farming/Levelup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farming
+{
+    class Levelup
+    {
+        public static void LU(int lvl, int xp, int levelup, string topic)
+        {
+        levelup:
+            Program.Clear();
+            lvl++;
+            xp = xp - levelup;
+            levelup = (lvl * 3 / 2) * levelup;
+            Console.WriteLine("-- Username: {0} - Balance: {1} - Level: {2} - Experience: {3} - Acres Owned: {4}", user, bal, lvl, xp, land);
+            Program.WL("");
+            Console.WriteLine("LEVEL UP! You have just leveled up to level {0}", lvl);
+            Program.WL("");
+            Program.Write("Press enter to continue to the last menu...");
+            Program.Read();
+
+            if (xp >= levelup)
+            {
+                goto levelup;
+            }
+
+            switch (topic)
+            {
+                case "farm":
+                    goto farm;
+                case "buy":
+                    goto buy;
+                case "sell":
+                    goto sell;
+                case "menu":
+                default:
+                    goto menu;
+            }
+        }
+    }
+}
---
+++ Farming/Farming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static Farming.Methods;
+using static Farming.Variables;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Farming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Welcome();
+        }
+    }
+}
